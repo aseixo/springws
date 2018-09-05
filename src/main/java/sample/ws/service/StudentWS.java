03c8135fb2efc540795ab71adb5773ed619eed51
aseixo <aseixo@zoho.com>
2018-09-05 18:02:59 +0200

    Changes in Wrappers, namely in className

--- src/main/java/sample/ws/service/StudentWS.java
+++ src/main/java/sample/ws/service/StudentWS.java
@@ -1,12 +1,47 @@
 package sample.ws.service;
 
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+import javax.xml.ws.RequestWrapper;
+import javax.xml.ws.ResponseWrapper;
 
 import sample.ws.model.Student;
 
-@WebService(targetNamespace= "http://service.ws.sample", name = "StudentWebService")
+@WebService(targetNamespace = "http://service.ws.sample/", name = "StudentWS")
+@SOAPBinding(style=Style.RPC, use=Use.LITERAL)
 public interface StudentWS {
-	
-	public Student getStudentById(long id);
 
+	@WebResult(name = "return", targetNamespace = "")
+
+	@RequestWrapper(localName = "getStudentByIdRequest", 
+		targetNamespace = "http://service.ws.sample/", 
+		className = "java.lang.Long")
+
+	@WebMethod(action = "urn:GetStudentById")
+
+	@ResponseWrapper(localName = "getStudentByIdResponse", 
+		targetNamespace = "http://service.ws.sample/",
+		className = "sample.ws.model.Student")
+	
+
+	public Student getStudentById(@WebParam(name = "id", targetNamespace = "") Long id);
+	
+	@WebResult(name = "return", targetNamespace = "")
+
+	@RequestWrapper(localName = "getServiceNameRequest", 
+		targetNamespace = "http://service.ws.sample/")
+
+	@WebMethod(action = "urn:GetServiceName")
+
+	@ResponseWrapper(localName = "getServiceNameResponse", 
+		targetNamespace = "http://service.ws.sample/",
+		className = "java.lang.String")
+	
+	
+	public String getServiceName();
 }
