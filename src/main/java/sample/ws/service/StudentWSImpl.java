ddf1c3a68099d1b983abe4db6d4201dede7758ad
aseixo <aseixo@zoho.com>
2018-09-08 15:57:24 +0200

    @Resource instead of @Autowired to fix ws exception

--- src/main/java/sample/ws/service/StudentWSImpl.java
+++ src/main/java/sample/ws/service/StudentWSImpl.java
@@ -1,14 +1,42 @@
 package sample.ws.service;
 
+import java.util.Date;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import sample.ws.bussines.StudentService;
 import sample.ws.model.Student;
 
-@javax.jws.WebService(serviceName = "StudentWebService", portName = "StudentPort", targetNamespace = "http://service.ws.sample/", endpointInterface = "sample.ws.service.StudentWS")
+
+@javax.jws.WebService(serviceName = "StudentWebService", 
+portName = "StudentPort", 
+targetNamespace = "http://ws.sample/service", 
+endpointInterface = "sample.ws.service.StudentWS")
 
 public class StudentWSImpl implements StudentWS {
 
-	public Student getStudentById(long id) {
+	private static Logger logger = LoggerFactory.getLogger(StudentWSImpl.class);
 
-		return new Student(2, "Olga María", "olgamfi@gmail.com", 3);
+	@Resource
+	private StudentService studentService;
+	
+	public Student getStudentById(Long id) {
+		
+		//ConverterUtils c = new ConverterUtils();
+		Student st = new Student();
+		st = this.studentService.getStudentById(id);
+		logger.info(st.toString());
+		return st;
 	}
 
+	@Override
+	public Student addStudent(Student s) {
+		
+		Student student =  studentService.add(s);
+		logger.info(student.toString());
+		return s;
+	}
 }
